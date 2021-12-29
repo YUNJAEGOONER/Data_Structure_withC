8deb9e7b819b51333443d7d655cdb7620bb82ff6
YUNJAEGOONER <50546745+YUNJAEGOONER@users.noreply.github.com>
2021-12-29 20:27:42 +0900

    Recursive_factorial
    재귀함수를 이용한 팩토리얼 계산기

---
+++ factorial_recursive.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int factorial(int n) { 
+	
+	if (n == 0) {
+		return 1;
+		//결과를 아는 0!이 될때까지
+		//0 ! = 1
+	}
+	else
+	{
+		return n * factorial(n - 1);
+	}
+}
+
+void main() {
+	printf("Factorial calculator\nInsert n :");
+	int n;
+	scanf_s("%d", &n);
+	int result = factorial(n);
+	printf("%d! = %d", n, result);
+}
