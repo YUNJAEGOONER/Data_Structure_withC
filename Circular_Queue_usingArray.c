630b98dd89090996b18ce95bbea9a2c3d193e29b
YUNJAEGOONER <50546745+YUNJAEGOONER@users.noreply.github.com>
2021-12-30 18:55:25 +0900

    Circular_Queue_using_array
    Array를 활용한 원형큐 만들기
    (기존코드 간략화)

--- Circular_Queue_usingArray.c
+++ Circular_Queue_usingArray.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-
 #define Max_Q_size 5
 //Queue의 사이즈를 8로 정의
-typedef int element;
-//자료형 int의 별칭을 element로 지정
-//int를 element라고 재정의
 
-element queue[Max_Q_size];
-element front = -1;
-element rear = -1;
+int queue[Max_Q_size];
+int front = -1;
+int rear = -1;
 
 int isEmpty() {
+
 	if (front == rear){
 		if (front != -1) {
 			if (queue[front%Max_Q_size] == NULL) {
@@ -25,20 +22,27 @@ int isEmpty() {
 		{
 			//front가 -1인경우
 			//아무것도 없는 큐
-			printf("Empty Queue");
+			printf("Empty Queue\n");
 		}
 	return 1;
 	}
+
 	else {
+		//Queue가 Empty가 아니다
+		//Queue에 Dequeue할수 있는 요소가 있다.
 		return 0;
 	}
 }
 
 int isFull() {
+
 	if ((rear - front)%Max_Q_size == Max_Q_size - 1){
-		printf("Full Queue");
+		printf("Full Queue\n");
+		return 1;
 	}
 	else {
+		//Queue가 Full이 아니다
+		//Queue에 Enqueue할 공간이 있다.
 		return 0;
 	}
 }
@@ -46,88 +50,54 @@ int isFull() {
 Enqueue(data) {
 
 	if (isFull() == 0) {
-		//Queue가 Full이 아닐경우
-
-		if (rear < Max_Q_size-1) {
-			if (rear == -1) {
-				front = front + 1;
-			}
-			rear = rear + 1;
-			queue[rear] = data;
+		//Queue가 Full이 아님 -> enqueue가능
+		if (rear == -1) {
+			front = front + 1;
 		}
-		else {
-			if (front % Max_Q_size == Max_Q_size - 1) {
-				front = front + 1;
-			}
-			rear = rear + 1;
-			queue[rear % Max_Q_size] = data;
+		else if (front % Max_Q_size == Max_Q_size - 1) {
+			front = front + 1;
 		}
+		rear = rear + 1;
+		queue[rear % Max_Q_size] = data;
 	}
-
-	}
+}
 
 Dequeue() {
-
 	if (isEmpty() == 0) {
-		if (front < Max_Q_size) {
-			queue[front] = NULL;
-			front = front + 1;
-		}
-		else {
-			queue[front % Max_Q_size] = NULL;
-			front = front + 1;
-		}
+		// Queue가 Empty가 아님 -> Dequeue가능
+		queue[front % Max_Q_size] = NULL;
+		front = front + 1;
 	}
-	else {
-
-	}
-	//Queue가 Empty가 아니라면
-	
-	//queue[front] = NULL;
-	//front = front + 1;
 }
 
 void main() {
-
-	Enqueue(1);
-	Enqueue(2);
-	Enqueue(3);
-	Enqueue(4);
-	Enqueue(5);
-	//Enqueue(6);
-
-	Dequeue();
-	printf("%d %d\n", front, rear);
-	Dequeue();
-	printf("%d %d\n", front, rear);
-	Dequeue();
-	printf("%d %d\n", front, rear);
-	Dequeue();
-	printf("%d %d\n", front, rear);
-	Dequeue();
-	printf("%d %d\n", front, rear);
-	
-
-	Dequeue();
 	Dequeue();
 	
-	//printf("%d %d %d", queue[4],rear,front);
+	for (int i = 0; i < Max_Q_size; i++) {
+		Enqueue(i + 1);
+	}
+	
+	for (int i = 0; i < Max_Q_size; i++) {
+		printf("%d번째로 Enqueue된 %d\n",i,queue[i]);
+	}
 
-	Enqueue(1);
-	Enqueue(2);
-	Enqueue(3);
-	Enqueue(4);
-	Enqueue(5);
 	Enqueue(6);
 
 	Dequeue();
 	Dequeue();
-	Dequeue();
-	Dequeue();
-	Dequeue();
 
-	Dequeue();
-	Dequeue();
-	Dequeue();
+	Enqueue(7);
+	Enqueue(8);
+
+	for (int i = 0; i < Max_Q_size; i++) {
+		if (queue[i] != NULL) {
+			printf("%d",queue[i]);
+		}
+	}
+	printf("\n");
+
+	for (int i = 0; i < Max_Q_size; i++) {
+		Dequeue();
+	}
 
 }
